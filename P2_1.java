49d53da14a6c8a5207b7b5063e1b97663ad8ac31
gaddamsundeep22 <59167166+gaddamsundeep22@users.noreply.github.com>
2020-01-27 14:40:30 +0530

    Add files via upload

---
+++ P2_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class P2_1{
+public static void main(String args[])
+{
+int c;
+Scanner sc=new Scanner(System.in);
+c=sc.nextInt();
+if(c>0)
+System.out.println("positive NUmber");
+else
+System.out.println("Negative Number");
+
+}
+}
